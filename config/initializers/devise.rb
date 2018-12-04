e52210b0acc63444ecf2662e3da906e3feca100d
padawanreas <rebeccaabiro@gmail.com>
2018-12-04 14:59:16 +0100

    tuto login fb for rails 5

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,7 +256,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, "1058549814318084", "b1885b06a9e063484d902d221344361c", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, 'API_KEY', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, 'GOOGLE_CLIENT_ID', 'GOOGLE_CLIENT_SECRET', {}
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
