f460ada41e21821a8803f54bc1dc7bb996213b29
padawanreas <rebeccaabiro@gmail.com>
2018-12-04 14:57:48 +0100

    tuto login fb for rails 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,28 +3,28 @@ class User < ApplicationRecord
   attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
- 
+
   validates :username, presence: true, uniqueness: {case_sensitive: false},
                                        format: {with: /\A[a-zA-Z0-9 _\.]*\z/}
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: [:facebook]
- 
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_hash).where("lower(username) = :value OR lower(email) = :value", value: login.downcase).first
-    else
-      where(conditions.to_hash).first
-    end
-  end
- 
-  def self.from_facebook(auth)
-    where(facebook_id: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.username = auth.info.name
-      user.password = Devise.friendly_token[0, 20]
-      user.skip_confirmation!
- 
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.new_with_session(params, session)
+ super.tap do |user|
+   if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+     user.email = data["email"] if user.email.blank?
+   end
+ end
+end
+
+def self.from_omniauth(auth)
+ where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+   user.email = auth.info.email
+   user.password = Devise.friendly_token[0,20]
+   user.name = auth.info.name   # assuming the user model has a name
+   user.image = auth.info.image # assuming the user model has an image
  end
 end
 end
