f76a32a2f25130ff35591b672dcea73b1320fb5e
padawanreas <rebeccaabiro@gmail.com>
2018-12-04 16:10:40 +0100

    conncetion via fb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+     member do
+       get :confirm_email
+     end
+   end
 end
--- db/migrate/20181203130015_devise_create_users.rb
+++ db/migrate/20181203130015_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :username,           null: false, default: ""
+
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,6 +36,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
        t.datetime :confirmation_sent_at
        t.string   :unconfirmed_email # Only if using reconfirmable
 
+
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
@@ -46,7 +47,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :username,             unique: true
+
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
