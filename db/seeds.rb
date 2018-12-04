a05330cf418773d0747d4366f171aa33dc0ba652
Maxime-Beaufils <thelimier@gmail.com>
2018-12-04 14:02:23 +0100

    create view index all game

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@cities = ["Paris","Marseille","Lyon","Toulouse","Nice","Nantes","Montpellier","Strasbourg",
+          "Bordeaux","Lille","Rennes","Reims","Le Havre","Saint-etienne","Toulon","Grenoble","Dijon","Angers","Nîmes","Villeurbanne","Saint-Denis",
+          "Le Mans","Aix-en-Provence","Clermont-Ferrand","Brest","Tours","Limoges" ,"Amiens" ,"Annecy","Perpignan" ,"Boulogne-Billancourt",
+          "Metz","Besançon","Orléans","Saint-Denis","Argenteuil","Mulhouse","Rouen","Montreuil","Caen"]
+
+200.times do
+  Game.create!(place: @cities.sample, date: "14/12/2018", game_format: "Legacy", description: "yogrogorgrg")
+end
