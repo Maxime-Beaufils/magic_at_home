8a4d8a7f571b0a971aa3f0df573b358a8b2e7c11
padawanreas <rebeccaabiro@gmail.com>
2018-12-04 16:10:00 +0100

    conncetion via fb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
  def facebook
    @user = User.from_omniauth(request.env["omniauth.auth"])
 
@@ -14,4 +15,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
  def failure
    redirect_to root_path
  end
+
 end
