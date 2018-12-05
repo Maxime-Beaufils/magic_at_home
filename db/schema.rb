0f557b67f8403e4fe92e8fe6f808c7aa3a378daa
Maxime-Beaufils <thelimier@gmail.com>
2018-12-05 15:39:34 +0100

    clean gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2018_12_04_101722) do
-=======
-ActiveRecord::Schema.define(version: 2018_12_03_130015) do
->>>>>>> devise
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "games", force: :cascade do |t|
     t.string "place"
     t.date "date"
@@ -27,11 +22,14 @@ ActiveRecord::Schema.define(version: 2018_12_03_130015) do
     t.bigint "user_creator_id"
     t.bigint "user_opponent_id"
     t.string "game_format"
+    t.float "latitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_creator_id"], name: "index_games_on_user_creator_id"
     t.index ["user_opponent_id"], name: "index_games_on_user_opponent_id"
-=======
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,7 +48,6 @@ ActiveRecord::Schema.define(version: 2018_12_03_130015) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> devise
   end
 
 end
