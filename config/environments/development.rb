9b24aeb1f8fb902d795ecc91d94e78c55e05561d
Maxime-Beaufils <thelimier@gmail.com>
2018-12-06 11:34:55 +0100

    fixed mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,10 +60,18 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Mailer
+
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+
  config.action_mailer.delivery_method = :smtp
  config.action_mailer.smtp_settings = {
-   address: 'localhost',
-   port: '1025'
- }
- config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV['gm_name'],
+  password:             ENV['gm_pass'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
 end
