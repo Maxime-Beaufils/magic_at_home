4ca9ab8c54c07d93b5e24d8c663dae0b1cd3b44f
padawanreas <rebeccaabiro@gmail.com>
2018-12-11 17:21:31 +0100

    fix conflit

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,22 @@ Rails.application.routes.draw do
   root 'landing_pages#index'
   get 'landing_pages/index'
 
-<<<<<<< HEAD
+
   resources :games, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :game_comments
   end
-=======
-  resources :games, only: [:index, :new, :create, :show, :edit, :update, :destroy]
->>>>>>> front_users
+
   post 'games/:id', to: 'games#join_game'
 
   resources :profile, only: [:index, :show, :edit, :update]
   get '/player', to: 'profile#index'
   get '/profil', to: 'profile#edit'
   patch '/profil', to: 'profile#update'
-  
+
   get '/map', to: 'dashboards#map'
 
 
-  devise_for :users, 
+  devise_for :users,
                     controllers: {
                                     confirmations: 'users/confirmations' ,
                                     omniauth_callbacks: "users/omniauth_callbacks" ,
