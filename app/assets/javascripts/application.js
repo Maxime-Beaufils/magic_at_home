fc89f4750d24592ded1066f21c5041cedf3e9e61
Maxime-Beaufils <thelimier@gmail.com>
2018-12-12 15:16:34 +0100

    initialize fullcalendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,29 +21,58 @@
 //= require bootstrap-notify
 //= require black-dashboard
 //= require moment
-//= require jquery_ujs
 //= require fullcalendar
 //= require fullcalendar/locale-all
 //= require_tree .
 
-function eventCalendar() {
-    return $('#calendar').fullCalendar({
-        
-    });
-  };
-  
-function clearCalendar() {
-    $('#calendar').fullCalendar('delete'); 
-    $('#calendar').html('');
-};
-
-jq321(document).on('turbolinks:load', function(){
-    eventCalendar();  
-  });
-  
-$(document).on('turbolinks:before-cache', clearCalendar);
+$('#calendar').fullCalendar({});
 
 function functionReveal() {
+    var initialize_calendar;
+initialize_calendar = function() {
+  $('.calendar').each(function(){
+    var calendar = $(this);
+    calendar.fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      selectable: true,
+      selectHelper: true,
+      editable: true,
+      eventLimit: true,
+      events: '/events.json',
+
+      select: function(start, end) {
+        $.getScript('/events/new', function() {});
+
+        calendar.fullCalendar('unselect');
+      },
+
+      eventDrop: function(event, delta, revertFunc) {
+        event_data = { 
+          event: {
+            id: event.id,
+            start: event.start.format(),
+            end: event.end.format()
+          }
+        };
+        $.ajax({
+            url: event.update_url,
+            data: event_data,
+            type: 'PATCH'
+        });
+      },
+      
+      eventClick: function(event, jsEvent, view) {
+        $.getScript(event.edit_url, function() {});
+      }
+    });
+  })
+};
+$(document).on('turbolinks:load', initialize_calendar);
+
   var x = document.getElementById("card-hide");
     if (x.style.display === "none") {
         x.style.display = "block";
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user_event_creator, class_name: "User", optional: true
   has_many :user_participant, through: :EventUsers, class_name: "User"
+
 end
