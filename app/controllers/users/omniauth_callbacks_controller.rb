cade3ff42488723bda3c58845e328d96df5d9e68
Bertrand-Pelletier <bertrand-888@hotmail.fr>
2018-12-13 16:34:25 +0100

    modif omniauth et routes.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,19 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
- def facebook
-   @user = User.from_omniauth(request.env["omniauth.auth"])
+def facebook
 
-   if @user.persisted?
-     sign_in_and_redirect @user, :event => :authentication
-     set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-   else
-     session["devise.facebook_data"] = request.env["omniauth.auth"]
-     redirect_to '/games'
-   end
- end
+@user = User.from_facebook(request.env['omniauth.auth'])
+if @user.persisted?
 
- def failure
-   redirect_to root_path
- end
+  sign_in_and_redirect @user, event: :authentication
+else
+  session['devise.facebook'] = request.env['omniauth.auth']
+  redirect_to new_user_registration_url
+end
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,11 +19,11 @@ Rails.application.routes.draw do
 
 
   devise_for :users,
-                    controllers: {
-                                    confirmations: 'users/confirmations' ,
-                                    omniauth_callbacks: "users/omniauth_callbacks" ,
-                                    passwords: 'users/passwords' ,
-                                    registrations: 'users/registrations' ,
-                                    sessions: 'users/sessions' ,
-                                    unlocks: 'users/unlocks' }
+  controllers: {
+       confirmations: 'users/confirmations' ,
+       omniauth_callbacks: 'users/omniauth_callbacks' ,
+       passwords: 'users/passwords' ,
+       registrations: 'users/registrations' ,
+       sessions: 'users/sessions' ,
+       unlocks: 'users/unlocks' }
 end
