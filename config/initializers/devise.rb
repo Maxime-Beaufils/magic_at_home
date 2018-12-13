aaf5bd657478755e05759d2a8fe62ec20f0766f1
Bertrand-Pelletier <bertrand-888@hotmail.fr>
2018-12-13 18:45:55 +0100

    modif omniauth Max

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,7 +257,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   
-  config.omniauth :facebook, Rails.application.credentials[:facebook_app_id], Rails.application.credentials[:facebook_app_secret], scope: 'email', info_fields: 'email,name',  callback_url: "https://magic-at-home-dev.herokuapp.com/users/auth/facebook/callback"
+  config.omniauth :facebook, ENV["fb_KEY"], ENV["fb_secret"], scope: 'user,public_repo'
+  #callback_url: "https://magic-at-home-dev.herokuapp.com/users/auth/facebook/callback"
   #config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
