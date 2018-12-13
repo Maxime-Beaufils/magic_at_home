f1bc027db889219abe7f60b0a4ac05aff36c01c1
Bertrand-Pelletier <bertrand-888@hotmail.fr>
2018-12-13 16:04:04 +0100

    ajout callback dans devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,7 +256,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV["fb_KEY"], ENV["fb_secret"], scope: 'user,public_repo'
+  config.omniauth :facebook, ENV["fb_KEY"], ENV["fb_secret"], scope: 'user,public_repo',
+  callback_url: https://magic-at-home-dev.herokuapp.com/users/auth/facebook
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
