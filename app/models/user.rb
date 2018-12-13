f4e02ce999f824abafa6c27efd21806b8ad25576
Bertrand-Pelletier <bertrand-888@hotmail.fr>
2018-12-13 17:50:10 +0100

    modif user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, omniauth_providers: [:facebook]
 
   def self.new_with_session(params, session)
     super.tap do |user|
@@ -19,12 +19,14 @@ class User < ApplicationRecord
     end
 end
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      #user.name = auth.info.name   # assuming the user model has a name
-      #user.image = auth.info.image # assuming the user model has an image
-      end
-    end
+def self.from_facebook(auth)
+  where(facebook_id: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.username = auth.info.name
+    user.password = Devise.friendly_token[0,20]
+    parse_name(user, auth.info.name) # assuming the user model has a name
+    user.image = auth.info.image # assuming the user model has an image
+    user.skip_confirmation!
+  end
+end
   end
