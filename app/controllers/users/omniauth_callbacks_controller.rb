edacdbe2d204ee9089dca1ab523719d09856db0e
Bertrand-Pelletier <bertrand-888@hotmail.fr>
2018-12-13 18:30:23 +0100

    suppresion alias facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def all
+  def facebook
+    puts "1"
     user = User.from_omniauth(request.env["omniauth.auth"], request.env["omniauth.params"])
+    puts "2"
     if user.persisted?
       flash.notice = "Signed in!"
       sign_in_and_redirect(user) and return
     else
+    puts "3"
       user.save
       flash.notice = "Created account!"
       sign_in_and_redirect(user) and return
     end
   end
- 
-
-  alias_method :facebook, :all
 
  end
