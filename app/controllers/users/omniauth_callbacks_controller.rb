49cc660ec5a7707ac3be31413833a6a378c0ff18
Bertrand-Pelletier <bertrand-888@hotmail.fr>
2018-12-13 18:21:16 +0100

    nouveau code omniauth_controller Max

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def facebook
-
-    @user = User.from_facebook(request.env['omniauth.auth'])
-    
-
-      sign_in_and_redirect @user, event: :authentication
-  
+  def all
+    user = User.from_omniauth(request.env["omniauth.auth"], request.env["omniauth.params"])
+    if user.persisted?
+      flash.notice = "Signed in!"
+      sign_in_and_redirect(user) and return
+    else
+      user.save
+      flash.notice = "Created account!"
+      sign_in_and_redirect(user) and return
+    end
   end
+ 
 
+  alias_method :facebook, :all
 
-end
+ end
